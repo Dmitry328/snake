461674ffc5183b889514e62cd837f320fe3824bf
Dmitry <dima.fic@mail.ru>
2017-01-30 15:25:32 +0300

    создал функцию Drow

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,26 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int x1 = 1;
+            int y1 = 3;
+            char sim1 = '*';
+
+            Drow(x1, y1, sim1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sim2 = '#';
+
+            Drow(x2, y2, sim2);
+             
             Console.ReadLine();
+
+            
+        }
+        static void Drow(int x, int y, char sim)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sim);
         }
     }
 }
