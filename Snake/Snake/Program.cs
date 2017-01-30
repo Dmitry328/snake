167c56409eb0b43f0c6070fb2e94ae87fe263e05
Dmitry <dima.fic@mail.ru>
2017-01-30 20:03:46 +0300

    Создание списков

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,7 +16,37 @@ namespace Snake
 
             Point p2 = new Point(4, 5, '#');
             p2.Drow();
-                  
+
+            Point p3 = new Point(6, 8, '_');
+            p3.Drow();
+
+            Point p4 = new Point(9, 10, '^');
+            p4.Drow();
+
+
+            List<int> namList = new List<int>();
+            namList.Add(0);
+            namList.Add(1);
+            namList.Add(2);
+
+            foreach(int i in namList)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            List<Point> pList = new List<Point>();
+
+
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach(char i in namList)
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadLine();
 
         }
